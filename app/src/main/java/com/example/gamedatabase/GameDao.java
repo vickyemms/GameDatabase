a88d0315cc19e7924397b36a044200e610347538
vickyemms <64374691+vickyemms@users.noreply.github.com>
2023-04-17 22:35:32 +0200

    added add, get and delete functions and implemented them

--- app/src/main/java/com/example/gamedatabase/GameDao.java
+++ app/src/main/java/com/example/gamedatabase/GameDao.java
@@ -1,12 +1,26 @@
 package com.example.gamedatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameDao extends SQLiteOpenHelper {
+
+    public static final String GAME_TABLE = "GAME_TABLE";
+    public static final String COLUMN_GAME_NAME = "GAME_NAME";
+    public static final String COLUMN_GAME_YEAR = "GAME_YEAR";
+    public static final String COLUMN_GAME_MIN_PLAYERS = "GAME_MIN_PLAYERS";
+    public static final String COLUMN_GAME_MAX_PLAYERS = "GAME_MAX_PLAYERS";
+    public static final String COLUMN_GAME_GENRE = "GAME_GENRE";
+    public static final String COLUMN_ID = "ID";
+
 
     public GameDao(@Nullable Context context) {
         super(context, "game.db", null, 1);
@@ -14,6 +28,12 @@ public class GameDao extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTableStatement = "CREATE TABLE " + GAME_TABLE +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + COLUMN_GAME_NAME + " TEXT, " + COLUMN_GAME_YEAR + " INT, " + COLUMN_GAME_MIN_PLAYERS +
+                " INT, " + COLUMN_GAME_MAX_PLAYERS + " INT, " + COLUMN_GAME_GENRE + " TEXT)";
+
+        sqLiteDatabase.execSQL(createTableStatement);
 
     }
 
@@ -21,4 +41,76 @@ public class GameDao extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
+    public boolean addGame(Game game){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_GAME_NAME, game.getName());
+        cv.put(COLUMN_GAME_YEAR, game.getYear());
+        cv.put(COLUMN_GAME_MIN_PLAYERS, game.getMinPlayers());
+        cv.put(COLUMN_GAME_MAX_PLAYERS, game.getMaxPlayers());
+        cv.put(COLUMN_GAME_GENRE, game.getGenre());
+
+        long insert = db.insert(GAME_TABLE, null, cv);
+        if(insert == -1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean deleteGame(Game game){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String queryString = "DELETE FROM " + GAME_TABLE + " WHERE " + COLUMN_ID + " = " + game.getId();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst()){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public List<Game> getAllGames(){
+
+        List<Game> gameList = new ArrayList<>();
+
+        String queryString = "SELECT * FROM " + GAME_TABLE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst()){
+            do {
+                int gameID = cursor.getInt(0);
+                String name = cursor.getString(1);
+                int year = cursor.getInt(2);
+                int minPlayers = cursor.getInt(3);
+                int maxPlayers = cursor.getInt(4);
+                String genre = cursor.getString(5);
+
+                Game game = new Game(gameID, name, year, minPlayers, maxPlayers, genre);
+
+                gameList.add(game);
+            } while(cursor.moveToNext());
+
+        } else {
+
+        }
+
+        cursor.close();
+        db.close();
+
+        return gameList;
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/gamedatabase/MainActivity.java
+++ app/src/main/java/com/example/gamedatabase/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -11,12 +13,17 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText etName, etYear, etMinPlayers, etMaxPlayers;
-    Button btnViewAll, btnAdd;
+    Button btnAdd;
     RadioGroup rgGenres;
     ListView lvGameList;
+
+    GameDao gameDao;
+    ArrayAdapter gameArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,36 +35,57 @@ public class MainActivity extends AppCompatActivity {
         etMinPlayers = findViewById(R.id.etMinPlayers);
         etMaxPlayers = findViewById(R.id.etMaxPlayers);
         rgGenres = findViewById(R.id.rgGenres);
-        btnViewAll = findViewById(R.id.btnViewAll);
         btnAdd = findViewById(R.id.btnAdd);
         lvGameList = findViewById(R.id.lvGameList);
+
+        gameDao = new GameDao(MainActivity.this);
+
+        showGamesOnListView(gameDao.getAllGames());
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                Game game;
+
                 try {
                     String genre = ((RadioButton)findViewById(rgGenres.getCheckedRadioButtonId())).getText().toString();
 
-                    Game game = new Game(-1, etName.getText().toString(),
+                    game = new Game(-1, etName.getText().toString(),
                             Integer.parseInt(etYear.getText().toString()),
                             Integer.parseInt(etMinPlayers.getText().toString()),
                             Integer.parseInt(etMaxPlayers.getText().toString()), genre);
-
-                    Toast.makeText(MainActivity.this, game.toString(), Toast.LENGTH_LONG).show();
                 }
                 catch (Exception e){
-                    Toast.makeText(MainActivity.this, "Missing input", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Missing input", Toast.LENGTH_SHORT).show();
+                    game = new Game(-1, "error", 0, 0, 0, "error");
                 }
+
+                GameDao gameDao = new GameDao(MainActivity.this);
+
+                gameDao.addGame(game);
+
+                showGamesOnListView(gameDao.getAllGames());
+
+                Toast.makeText(MainActivity.this, "Added new game", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        btnViewAll.setOnClickListener(new View.OnClickListener() {
+        lvGameList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "View All", Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Game game = (Game)adapterView.getItemAtPosition(position);
+                gameDao.deleteGame(game);
+                showGamesOnListView(gameDao.getAllGames());
+                Toast.makeText(MainActivity.this, "Deleted game", Toast.LENGTH_SHORT).show();
             }
         });
 
+    }
+
+    private void showGamesOnListView(List<Game> allGames) {
+        gameArrayAdapter = new ArrayAdapter<Game>(MainActivity.this, android.R.layout.simple_list_item_1, allGames);
+        lvGameList.setAdapter(gameArrayAdapter);
     }
 }
